5c581177cc074426034a801c9e504a62fd8fd702
ediazva <ediazva@unsa.edu.pe>
2024-07-10 16:33:43 -0500

    dashboard.html

---
+++ src/main/resources/static/scripts/menu.js
@@ -0,0 +1,57 @@
+function create_div(content) {
+  return `<div class="d-inline-block rounded border p-3 bg-white">${content}</div>`;
+}
+
+function default_div() {
+  return create_div('<span class="spinner-border text-slider"></span>');
+}
+
+const _MENUS_HTML = {
+  0: default_div(),
+  1: default_div(),
+  2: default_div(),
+  3: default_div(),
+};
+
+let _ACTUAL_ID = 0;
+
+function save_profile() {
+  _MENUS_HTML[0] = $("#main-content").html();
+}
+
+function reload_menu(id) {
+  if(id == _ACTUAL_ID)
+    $("#main-content").html(_MENUS_HTML[_ACTUAL_ID]);
+}
+
+/**
+ * 
+ * @param {HTMLButtonElement} self 
+ * @param {*} id
+ */
+function change_menu(self, id) {
+  $(".active").removeClass("active");
+  self.classList.add("active");
+
+  if(id in _MENUS_HTML) {
+    $("#main-content").html(_MENUS_HTML[id]);
+    _ACTUAL_ID = id;
+    if(id != 0) {
+      $("#table").DataTable(
+        {
+          data: _TABLES_CACHE[id],
+          columns: _TABLES_COLUMNS[id][0],
+          columnDefs: _TABLES_COLUMNS[id][1],
+          language: _TABLE_LANGUAGE
+        }
+      );
+    }
+  } else
+    console.error(`Request menu id ${id} don't exists`);
+}
+
+function logout() {
+  window.location.href = "/";
+}
+
+$(save_profile);
--- src/main/resources/static/scripts/tables.js
+++ src/main/resources/static/scripts/tables.js
@@ -1,23 +1,91 @@
-const TABLES_CACHE = {};
+function course_capitalize(str) {
+  str = str.toLowerCase();
+  const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+  return str.split(" ").map((str) => {
+      if(str.search("^[ivIV]+$") != -1)
+          return str.toUpperCase();
+      return str.length > 3 ? capitalize(str) : str;
+  }).join(" ");
+}
 
-function loadAllTables(cui) {
+const _TABLES_CACHE = {};
+const _TABLE_MATRICULADOS_COLUMNS = [
+  {title: "Código", data: "codigo"},
+  {title: "Créditos", data: "creditos"},
+  {title: "Curso", data: "nombre"},
+  {title: "Docente", data: "prof_apellido"},
+  {data: "prof_nombre"},
+  {title: "Semestre", data: "semestre"},
+];
+const _TABLE_MATRICULADOS_COLUMNSDEF = [
+  {
+    render: (data, row, type) => course_capitalize(data),
+    targets: 2
+  },
+  {
+    render: (data, row, type) => type["prof_nombre"] + " " + data,
+    targets: [3,4]
+  },
+  {
+    visible: false,
+    targets: 4
+  }
+];
+const _TABLE_MATRICULABLES_COLUMNS = [
+  {title: "Código", data: "codigo"},
+  {title: "Curso", data: "nombre"},
+  {title: "Semestre", data: "semestre"},
+  {title: "Créditos", data: "creditos"},
+];
+const _TABLE_MATRICULABLES_COLUMNSDEF = [
+  {
+    render: (data, row, type) => course_capitalize(data),
+    targets: 1
+  }
+];
+const _TABLE_LANGUAGE = {
+  lengthMenu: "Mostrar _MENU_ registros por página",
+  zeroRecords: "Ningún registro encontrado",
+  info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+  infoEmpty: "Ningún registro encontrado",
+  infoFiltered: "(filtrados desde _MAX_ registros totales)",
+  search: "Buscar:",
+  loadingRecords: "Cargando...",
+  paginate: {
+    first: "Primero",
+    last: "Último",
+    next: "Siguiente",
+    previous: "Anterior"
+  }
+};
+
+const _TABLES_COLUMNS = {
+  2: [_TABLE_MATRICULADOS_COLUMNS, _TABLE_MATRICULADOS_COLUMNSDEF],
+  3: [_TABLE_MATRICULABLES_COLUMNS, _TABLE_MATRICULABLES_COLUMNSDEF],
+};
+
+function loadAllTables() {
   const rest_tables = [
-    "/aprobados",
-    "/reprobados",
-    "/promedios",
-    "/matriculados",
-    "/matriculables",
-    "/historial",
+    ["/matriculados", 2],
+    ["/matriculables", 3],
   ];
 
   rest_tables.forEach(table => $.ajax(
     {
-      url: table,
+      url: table[0],
       method: "get",
       data: {
-        cui,
+        cui: _CUI
       }
-    }).done(data => TABLES_CACHE[table] = data));
+    }
+  ).done(data => {
+    const id = table[1];
+    _MENUS_HTML[id] = create_div('<table class="table" id="table"></table>');
+    _TABLES_CACHE[id] = data;
+    reload_menu(id);
+  }).fail(() => {
+    _MENUS_HTML[table[1]] = create_div(`Error al obtener table del url ${table[0]}`);
+  }));
 }
 
-loadAllTables(cui);
+loadAllTables();
